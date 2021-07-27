5d4f2ebd6a8eb6999479001890f3012b71933805
Mauro Gandulfo <maurogandulfo@hotmail.com>
2021-07-27 20:32:02 +0200

    Modification of the responses structures

--- index.js
+++ index.js
@@ -2,22 +2,26 @@
 
 const responseSuccess = {
   statusCode: 200,
-  body: JSON.stringify({
+  body: {
+    responseType: "Success",
     message: "ok",
-  }),
+  },
 };
 
 const responseError = {
   statusCode: 400,
-  body: JSON.stringify({
+  body: {
+    responseType: "Error",
     message: "error",
-  }),
+  },
 };
 
 exports.handler = (event, context, callback) => {
   if (error) {
+    responseError.body = JSON.stringify(responseError.body);
     callback(responseError);
   } else {
+    responseSuccess.body = JSON.stringify(responseSuccess.body);
     callback(undefined, responseSuccess);
   }
 };
